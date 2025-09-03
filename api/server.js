3eee0832fbbeef9e6b558802592e5a7a37ecde7f
Luca Roszak <luca.roszak@treestack.de>
2025-09-03 11:31:44 +0200

    fix: prettiefy JavaScript Files

--- api/server.js
+++ api/server.js
@@ -1,23 +1,33 @@
 const express = require("express");
-const app = express()
-const PORT = 8080
+const app = express();
+const PORT = 8080;
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/cpu-move", (req, res) => {
-    const { board } = req.body;
+  const { board } = req.body;
 
-    if(!board){
-        res.status(418).send({message: "no board provided"})
-    }
-    
-    res.status(200).send(
-        {
-            board: "test"
-        }
-    )
-})
+  if (!board) {
+    res.status(418).send({ message: "no board provided" });
+  }
+
+  res.status(200).send({
+    board: "test",
+  });
+});
+
+app.post("/check-game", (req, res) => {
+  const { board } = req.body;
+
+  if (!board) {
+    res.status(418).send({ message: "no board provided" });
+  }
+
+  res.status(200).send({
+    board: "test",
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is Running on http://localhost:${PORT}`)
+  console.log(`Server is Running on http://localhost:${PORT}`);
 });
