3fba877be533bb668df32432f46944fb36b4174c
Luca Roszak <luca.roszak@treestack.de>
2025-09-03 17:21:35 +0200

    feat: implement api for gamestate checks and cpu controlled moves

---
+++ src/api/server.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const { resolve } = require("path");
+const app = express();
+const PORT = 8080;
+
+const allowedOrigins = ["http://127.0.0.1:3000"];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+app.use(express.json());
+
+function checkPotential(a, board, response) {
+  for (let x = 0; x <= 2; x++) {
+    if (board[x][0] + board[x][1] + board[x][2] == a) {
+      if (!response) {
+        for (let y = 0; y <= 2; y++) {
+          if (board[x][y] == 0) {
+            response = { move: [y, x] };
+          }
+        }
+      }
+    }
+
+    if (board[0][x] + board[1][x] + board[2][x] == a) {
+      if (!response) {
+        for (let y = 0; y <= 2; y++) {
+          if (board[y][x] == 0) {
+            response = { move: [x, y] };
+          }
+        }
+      }
+    }
+  }
+
+  if (board[0][0] + board[2][2] + board[1][1] == a && !response) {
+    if (!response) {
+      for (let x = 0; x <= 2; x++) {
+        if (board[x][x] == 0) {
+          response = { move: [x, x] };
+        }
+      }
+    }
+  }
+
+  if (board[0][2] + board[2][0] + board[1][1] == a && !response) {
+    if (!response) {
+      for (let x = 0; x <= 2; x++) {
+        if (board[x][2 - x] == 0) {
+          response = { move: [2 - x, x] };
+        }
+      }
+    }
+  }
+
+  return response;
+}
+
+app.post("/cpu-move", (req, res) => {
+  const { board } = req.body;
+
+  let response;
+
+  response = checkPotential(-2, board, response);
+
+  if (!response) {
+    response = checkPotential(2, board, response);
+  }
+
+  if (!response) {
+    let possible = [];
+    for (let x = 0; x <= 2; x++) {
+      for (let y = 0; y <= 2; y++) {
+        if (board[x][y] == 0) {
+          possible.push([y, x]);
+        }
+      }
+    }
+    response = { move: possible[Math.floor(Math.random() * possible.length)] };
+  }
+
+  console.log(response);
+
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(response);
+});
+
+app.post("/check-game", (req, res) => {
+  const { board } = req.body;
+  const { turn } = req.body;
+
+  var response;
+  var done = false;
+
+  if (!board) {
+    res.status(418).send({ message: "no board provided" });
+  }
+
+  for (let x = 0; x < 3; x++) {
+    if (
+      Math.abs(board[x][0] + board[x][1] + board[x][2]) == 3 ||
+      Math.abs(board[0][x] + board[1][x] + board[2][x]) == 3
+    ) {
+      response = { result: 1 };
+      done = true;
+    }
+  }
+
+  if (!done && Math.abs(board[0][0] + board[1][1] + board[2][2]) == 3) {
+    response = { result: 1 };
+    done = true;
+  }
+
+  if (!done && Math.abs(board[2][0] + board[1][1] + board[0][2]) == 3) {
+    response = { result: 1 };
+    done = true;
+  }
+
+  if (!done && turn == 8) {
+    response = { result: -1 };
+  }
+
+  if (!response) {
+    response = { result: 0 };
+  }
+
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(response);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
---
+++ src/ui/index.js
@@ -0,0 +1,111 @@
+let h2 = document.getElementById("turn");
+
+let turn = 0;
+
+let gameOver = false;
+
+let board = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+async function clicked(b, x, y) {
+  if (turn % 2 == 0) {
+    h2.textContent = `Player ${2 - (turn % 2)}\'s Turn`;
+
+    if (turn % 2 == 0) {
+      b.style.background = "#00FF00";
+      board[x][y] = 1;
+    } else {
+      b.style.background = "#FF0000";
+      board[x][y] = -1;
+    }
+
+    b.disabled = true;
+    await checkGame();
+
+    if (!gameOver && turn % 2 == 1) {
+      cpuTurn();
+    }
+  }
+}
+
+async function cpuTurn() {
+  await fetch("http://localhost:8080/cpu-move", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ board: board }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const num = 3 * data["move"][1] + data["move"][0] + 1;
+      let buttons = document.querySelectorAll(".b");
+
+      console.log(num);
+
+      let counter = 1;
+
+      buttons.forEach((button) => {
+        if (counter == num) {
+          button.style.background = "#FF0000";
+          button.disabled = true;
+          board[data["move"][0]][["move"][1]] = -1;
+        }
+        counter++;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  await checkGame();
+}
+
+function gameEnd(a) {
+  gameOver = true;
+  let buttons = document.querySelectorAll(".b");
+
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+
+  if (a) {
+    h2.textContent = `It's a Tie`;
+    return;
+  }
+  h2.textContent = `Player ${(turn % 2) + 1} Wins`;
+}
+
+async function checkGame() {
+  await fetch("http://localhost:8080/check-game", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ board: board, turn: turn }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (!data["result"] == 0) {
+        gameEnd(data["result"] == -1);
+        turn--;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  turn++;
+}
